872c3b2003b26a0d07f5ffb4ebbb4b8973c40257
Darth Vader <ayimdomnic@outlook.com>
2016-06-21 20:05:51 +0300

    Fixes on the Adventure model

--- app/Adventure.php
+++ app/Adventure.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Adventure extends Model
 {
-    //
+    protected $fillable = ['story_id','telegram_chat_id','last_updated','current_page', 'active_complete', 'active', 'complete'];
+
+
+    //relationships
+
+    public function story()
+    {
+    	return story::find($this->story_id);
+    }
+
+    public function
 }
