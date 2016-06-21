af65d6ebc3d4470dc7aff8bff786b3cf137ae815
Darth Vader <ayimdomnic@outlook.com>
2016-06-21 20:52:44 +0300

    Fixes on the Webhook Controller, Sketchy I know

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,5 +8,13 @@ use App\Http\Requests;
 
 class WebhookController extends Controller
 {
-    //
+    public function webhook(Request $request)
+    {
+    	//include the neccessary packages
+    	$update = Telegram::commandsHandler(true);
+
+    	Log::info('Webhook Received:' . json_encode($update));
+
+        return 'OK';
+    }
 }
