ab07886f4605e889b4533e3dd11973e70492374b
Darth Vader <ayimdomnic@outlook.com>
2016-06-21 20:21:55 +0300

    Page Model on steroids, Syntax errors is what? :)

--- app/Page.php
+++ app/Page.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     //
+    protected $fillable = [
+    	'title', 'content','story_id',
+    ];
+
+    public function story()
+    {
+    	return $this->belongsTo(Story::class);
+    }
+
+    public function actions()
+    {
+    	return $this->hasMany(Action::class);
+    }
+
+    public function reply_markup()
+
+    return \Telegram::replyKeyboardMarkup([
+    	'keyboard' => PageBuilder::convertsActionsToButtons($this->actions),
+    	'resize_keyboard' => true,
+    	'one_time_keyboard'=>true
+    	
+    	]);
 }
